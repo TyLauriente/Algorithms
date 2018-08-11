af6cca6071a3eee851a2584db12694b246fb0950
Ty lauriente <tlauriente@gmail.com>
2018-08-11 19:56:03 +0000

    [VS] - FIXED FUCKING MOVE LINE Imported from Changeset 182

--- Algorithms/Stack/main.cpp
+++ Algorithms/Stack/main.cpp
@@ -1,28 +1,22 @@
 #include<iostream>
+#include<string>
+#include<fstream>
 #include"Stack.h"
 #include"LinkedList.h"
 #include"Queue.h"
 
 using namespace std;
 
+string GetStringOfFile(string fileName)
+{
+	ofstream(fileName);
+
+
+}
+
 int main()
 {
-	Queue<int>* test1 = new Queue<int>();
-
-	test1->Push(0);
-	test1->Push(1);
-	test1->Push(2);
-
-	Queue<int> test = std::move(*test1);
 	
-	//test = *test1;
-
-	delete test1;
-
-	for (int index = 0; index < 3; ++index)
-	{
-		cout << test.Pop() << endl;
-	}
 
 	system("pause");
 	return 0;

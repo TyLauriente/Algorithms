1f19e2c475fcb84459dfa8064b7de3c9aca44efa
Ty lauriente <tlauriente@gmail.com>
2018-08-11 20:49:07 +0000

    [Algoithms] - CodeErrorCheck main made Imported from Changeset 183

--- Algorithms/Stack/main.cpp
+++ Algorithms/Stack/main.cpp
@@ -7,16 +7,90 @@
 
 using namespace std;
 
+const int AMOUNT_OF_CHARACTERS = 3;
+
+char openSymbols[AMOUNT_OF_CHARACTERS] = { '(', '[', '{' };
+char closeSymbols[AMOUNT_OF_CHARACTERS] = { ')', ']', '}' };
+
+
+bool Contains(char arr[], char character)
+{
+	for (int index = 0; index < AMOUNT_OF_CHARACTERS; ++index)
+	{
+		if (arr[index] == character)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+char InvertChar(char symbol)
+{
+	for (int index = 0; index < AMOUNT_OF_CHARACTERS; ++index)
+	{
+		if (openSymbols[index] == symbol)
+		{
+			return closeSymbols[index];
+		}
+		else if (closeSymbols[index] == symbol)
+		{
+			return openSymbols[index];
+		}
+	}
+}
+
 string GetStringOfFile(string fileName)
 {
-	ofstream(fileName);
+	ifstream file(fileName);
 
+	string returnStr;
+	string temp;
 
+	while (getline(file, temp))
+	{
+		returnStr += temp;
+	}
+	return returnStr;
+}
+
+bool HasError(string code)
+{
+	Stack<char> stack;
+	
+	for (char c : code)
+	{
+		if (Contains(openSymbols, c))
+		{
+			stack.Push(c);
+		}
+		if (Contains(closeSymbols, c))
+		{
+			if (stack.count == 0 || stack.Pop() != InvertChar(c))
+			{
+				return true;
+			}
+		}
+	}
+	if (stack.count > 0)
+	{
+		return true;
+	}
+	return false;
 }
 
 int main()
 {
-	
+	string test = GetStringOfFile("code.txt");
+
+	if (HasError(test))
+	{
+		cout << "Has error" << endl;
+	}
+	else
+	{
+		cout << "Does not have any errors" << endl;
+	}
 
 	system("pause");
 	return 0;
